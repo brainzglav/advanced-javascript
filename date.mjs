c483f2593ee276b88615487c0efcf0ea0e229889
marzglav <feg.leg.crypto@gmail.com>
2022-04-04 19:15:21 +0200

    Importing with .mjs

---
+++ date.mjs
@@ -0,0 +1,14 @@
+/* import moment from "moment";
+
+const PRIMARY_DATE_FORMAT = "MMMM, DD HH:mm:ss YYYY";
+const SECONDARY_DATE_FORMAT = "YYYY-MM-DD";
+
+console.log(
+  moment().format(PRIMARY_DATE_FORMAT),
+  moment().format(SECONDARY_DATE_FORMAT)
+);
+ */
+
+import { HELLO_WORLD } from "./helper.mjs";
+
+console.log(HELLO_WORLD);
